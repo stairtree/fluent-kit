e8431d226d5fc109b4e6e4dfd3036550b7411be6
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-03-02 09:48:22 -0600

    Rely on Stairtree forks

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.1.0"),
-        .package(url: "https://github.com/vapor/async-kit.git", from: "1.0.0"),
+        .package(url: "https://github.com/stairtree/sql-kit.git", .branch("master")),
+        .package(url: "https://github.com/stairtree/async-kit.git", .branch("master")),
     ],
     targets: [
         .target(name: "FluentKit", dependencies: [
