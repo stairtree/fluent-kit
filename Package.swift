e8f7307a0833413ab52b755edc729509d2545937
Gwynne Raskind <gwynne@darkrainfall.org>
2021-04-04 12:02:16 -0500

    Update default branch name in sync workflow and Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/stairtree/sql-kit.git", .branch("master")),
-        .package(url: "https://github.com/stairtree/async-kit.git", .branch("master")),
+        .package(url: "https://github.com/stairtree/sql-kit.git", .branch("main")),
+        .package(url: "https://github.com/stairtree/async-kit.git", .branch("main")),
     ],
     targets: [
         .target(name: "FluentKit", dependencies: [
